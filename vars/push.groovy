602493c90cdf147d92dbc4f3b64bc940535c7651
Aayusha Regmi <108624943+Aayusha-Regmi@users.noreply.github.com>
2026-06-30 07:54:50 +0545

    Fix typo in usernamePassword credential usage

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,6 +1,6 @@
 def call(String idName, String imageName,String newImageName ){
 
-    withCredentials([usenamePassword(credentialId: "${idName}",usernameVariable: "USER", usernamePassword: "PASS")]){
+    withCredentials([usernamePassword(credentialsId: "${idName}",usernameVariable: "USER", passwordVariable: "PASS")]){
      sh " docker login -u ${env.USER} -p ${env.PASS}"
      sh "docker image tag ${imageName} ${env.USER}/${newImageName}"
      sh "docker push ${env.USER}/${newImageName}"
